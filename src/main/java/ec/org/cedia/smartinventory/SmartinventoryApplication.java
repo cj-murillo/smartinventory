9a37e256b0429d27d0d233928b739cbe076acce2
Jordan Murillo <jordan.murillo@cedia.org.ec>
2026-05-14 20:41:41 -0500

    chore: proyecto base SmartInventory generado con Spring Initializr

---
+++ src/main/java/ec/org/cedia/smartinventory/SmartinventoryApplication.java
@@ -0,0 +1,13 @@
+package ec.org.cedia.smartinventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartinventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartinventoryApplication.class, args);
+	}
+
+}
